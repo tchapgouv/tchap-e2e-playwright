5f76231f68f40047f51e76ea35f8b21c442f0fab
Olivier D <odelcroi@gmail.com>
2025-04-22 10:11:36 +0200

    Update start-local-mas.sh

--- start-local-mas.sh
+++ start-local-mas.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 set -e
+
+# Check if MAS_HOME is defined
+if [ -z "$MAS_HOME" ]; then
+    echo "Error: MAS_HOME environment variable is not defined"
+    echo "Please set MAS_HOME to the path of your matrix-authentication-service directory"
+    exit 1
+fi
+
 MAS_CONF=$PWD/conf
 cd $MAS_HOME
 cargo run -- server -c $MAS_CONF/config.local.dev.yaml
