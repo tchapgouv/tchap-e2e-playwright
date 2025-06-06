14c186524a3dd58a8345134f2f13dffff243dd55
olivier <odelcroi@gmail.com>
2025-06-06 10:50:36 +0200

    fix proconnect button test

--- playwright/tests/utils/auth-helpers.ts
+++ playwright/tests/utils/auth-helpers.ts
@@ -48,7 +48,9 @@ export async function performOidcLogin(page: Page, user: TestUser, screenshot_pa
   
   // Find and click the OIDC provider button (adjust the selector as needed)
   // This is based on the login.html template which shows provider buttons
-  const oidcButton = page.locator('a.cpd-button[href*="/upstream/authorize/"]');
+  //const oidcButton = page.locator('a.cpd-button[href*="/upstream/authorize/"]');
+  //catch proconnect button with class proconnect-button
+  const oidcButton = page.locator('button.proconnect-button');
   await oidcButton.click();
   
   // Wait for navigation to Keycloak
