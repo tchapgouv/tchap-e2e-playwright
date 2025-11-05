798ee57e5c122b2f764a195cf80137dd0cf285d5
Olivier D <odelcroi@gmail.com>
2025-11-05 10:55:41 +0100

    add tests for register with password  (#15)
    * add tests for register
    
    * add check on email
    
    * add a new fixture to capture screenshot
    
    * fix fixture callable type
    
    * clean tests
    
    * test when no oauth2_authorization_grant

--- playwright/tests/auth/tchap-register-password.spec.ts
+++ playwright/tests/auth/tchap-register-password.spec.ts
@@ -3,73 +3,84 @@ import {
   extractVerificationCode
 } from '../../utils/auth-helpers';
 import { getMasUserByEmail } from '../../utils/mas-admin';
-import { SCREENSHOTS_DIR, ELEMENT_URL } from '../../utils/config';
+import {ELEMENT_URL } from '../../utils/config';
 
 
-test.describe('Tchap : register password', () => {
+test.describe('Tchap : register with password', () => {
 
-  test('tchap register with password', async ({ context, page, simpleUser: user }) => {
+  test('tchap register with oidc native', async ({ context, page, simpleUser: user, screenChecker: screen }) => {
     
     const email = user.kc_email;
+    let password = "sdf78qsd!9090ssss";
 
-    const screenshot_path = test.info().title.replace(" ", "_");
-  
     await page.goto(`${ELEMENT_URL}/#/welcome`, { waitUntil: 'networkidle' });
-  
-    //welcome
-    await page.waitForURL(url => url.toString().includes(`#/welcome`));
-    await page.screenshot({ path: `${SCREENSHOTS_DIR}/${screenshot_path}/01--tchap-login-page.png` });
+    
+    await screen(page, '#/welcome');
     await page.getByRole('link').filter({hasText : "Créer un compte"}).click();
-    await page.waitForURL(url => url.toString().includes(`#/email-precheck-sso`));
-    await page.screenshot({ path: `${SCREENSHOTS_DIR}/${screenshot_path}/01-tchap-precheck-sso.png` });
+
+    await screen(page, '#/email-precheck-sso');
     await page.locator('input').fill(email);
     await page.getByRole('button').filter({ hasText: 'Continuer' }).click();
   
-    //register
-    await page.waitForURL(url => url.toString().includes(`/register`));
-    await page.screenshot({ path: `${SCREENSHOTS_DIR}/${screenshot_path}/03-register.png`, fullPage: true });
-
-    //password
+    await screen(page, '/register');
     await page.getByRole('button').filter({ hasText: 'Continuer avec une adresse mail' }).click();
-    await page.waitForURL(url => url.toString().includes(`/register/password`));
-    await page.screenshot({ path: `${SCREENSHOTS_DIR}/${screenshot_path}/04-password.png`, fullPage: true });
-    let password = "sdf78qsd!9090ssss";
+
+    await screen(page, '/register/password');
+    await expect(page.locator('input[name="email"]')).toHaveValue(email);
     await page.locator('input[name="password"]').fill(password);
     await page.locator('input[name="password_confirm"]').fill(password);
     await page.getByRole('button').filter({ hasText: 'Continuer' }).click();
 
-    //verify-email
-    await page.waitForURL(url => url.toString().includes(`/verify-email`));
-    await page.screenshot({ path: `${SCREENSHOTS_DIR}/${screenshot_path}/05-verify_email.png`, fullPage: true });
-    let verificationCode  = await extractVerificationCode(context, screenshot_path);
-    console.log("verification code extracted : ", verificationCode);
+    await screen(page, '/verify-email');
+    let verificationCode  = await extractVerificationCode(context, screen);
     await page.locator('input[name="code"]').fill(verificationCode);
     await page.getByRole('button').filter({ hasText: 'Continuer' }).click();
 
-    //display-name
-    await page.waitForURL(url => url.toString().includes(`/display-name`));
-    await page.screenshot({ path: `${SCREENSHOTS_DIR}/${screenshot_path}/06-display-name.png`, fullPage: true });
+    await screen(page, '/consent');
     await page.getByRole('button').filter({ hasText: 'Continuer' }).click();
 
-    //consent
-    await page.waitForURL(url => url.toString().includes(`/consent`));
-    await page.screenshot({ path: `${SCREENSHOTS_DIR}/${screenshot_path}/07-consent.png`, fullPage: true });
-    await page.getByRole('button').filter({ hasText: 'Continuer' }).click();
-
-
-    //tchap
-    await page.waitForURL(url => url.toString().includes(`#/home`), { timeout: 20000 });
-    //TODO this condition is hardcoded
+    await screen(page, '#/home');
     await expect(page.locator('h1').filter({ hasText: /Bienvenue.*\[Tchapgouv\]/ })).toBeVisible({ timeout: 20000 });
-    await page.screenshot({ path: `${SCREENSHOTS_DIR}/${screenshot_path}/08-tchap-home.png`, fullPage: true });
-    
-
     const created_user = await getMasUserByEmail(email);
 
     //check created username fields
     expect(created_user.attributes.username).toContain(user.kc_username);
 
     //todo : check displayname? -> display name is stored in Synapse, or in the home screen of Tchap
+  });
+
+  test('tchap register with oidc native when login_hint is mistaken', async ({page, simpleUser: user, screenChecker: screen  }) => {
+    
+    const email = user.kc_email;
+    const second_email = user.kc_email.replace("@","another_email@");
+
+    await page.goto(`${ELEMENT_URL}/#/welcome`, { waitUntil: 'networkidle' });
+  
+    await screen(page, '#/welcome');
+    await page.getByRole('link').filter({hasText : "Créer un compte"}).click();
+    await screen(page, '#/email-precheck-sso');
+    await page.locator('input').fill(email);
+    await page.getByRole('button').filter({ hasText: 'Continuer' }).click();
+    await screen(page, '/register');
+    await page.getByRole('button').filter({ hasText: 'Continuer avec une adresse mail' }).click();
+
+    //click on link: wrong email
+    await screen(page, '/register/password');
+    await expect(page.locator('input[name="email"]')).toHaveValue(email);
+    await page.getByRole('link').filter({ hasText: 'pas la bonne adresse' }).click();
+
+    //input second email
+    await screen(page, '#/welcome');
+    await page.getByRole('link').filter({hasText : "Créer un compte"}).click();
+    await screen(page, '#/email-precheck-sso');
+    await page.locator('input').fill(second_email);
+    await page.getByRole('button').filter({ hasText: 'Continuer' }).click();
+    await screen(page, '/register');
+    await page.getByRole('button').filter({ hasText: 'Continuer avec une adresse mail' }).click();
+    
+    //check second email
+    await screen(page, '/register/password');
+    await expect(page.locator('input[name="email"]')).toHaveValue(second_email);
   });
 
 });
