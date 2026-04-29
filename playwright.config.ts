0b99037a5e19e2656d2a90b30611285961fe37f3
marc.sirisak <marc.sirisak@beta.gouv.fr>
2026-04-29 12:51:36 +0200

    Add new external flow in web

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ dotenv.config();
  * See https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Maximum time one test can run for */
   timeout: 15 * 1000,
   /* Run tests in files in parallel */
@@ -24,23 +24,23 @@ export default defineConfig({
 
   retries: process.env.CI ? 2 : 2,
   /* Reporter to use */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below */
   use: {
     /* Base URL to use in actions like `await page.goto('/')` */
-    baseURL: process.env.MAS_URL || 'https://auth.tchapgouv.com',
+    baseURL: process.env.MAS_URL || "https://auth.tchapgouv.com",
 
     /* Set locale to French */
     locale: BROWSER_LOCALE,
 
     /* Collect trace when retrying the failed test */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
 
     /* Take screenshot on failure */
-    screenshot: 'only-on-failure',
+    screenshot: "only-on-failure",
 
     /* Record video on failure */
-    video: 'on-first-retry',
+    video: "on-first-retry",
 
     /* Ignore HTTPS errors */
     ignoreHTTPSErrors: true,
@@ -58,9 +58,23 @@ export default defineConfig({
       use: { ...devices['Desktop Edge'] },
     },
     */
+
+    /* when using archlinux to use ui testing, bundled browser are not correctly installed
+     Directly use the installed chromium
+         {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"],
+              launchOptions: {
+                executablePath: "/usr/bin/chromium",
+              },
+      },
+    },
+      },
+
+    */
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
   ],
 });
--- utils/config.ts
+++ utils/config.ts
@@ -2,11 +2,17 @@ import dotenv from 'dotenv';
 import path from 'node:path';
 
 // Determine which environment to use
-export const env = process.env.ENV || 'local';
+export const env = process.env.ENV;
 console.log(`Loading environment configuration for: ${env}`);
 
-// Load environment variables from the appropriate .env file
-dotenv.config({ path: path.resolve(__dirname, `../.env.${env}`) });
+// if no env is set, just take .env
+if (!env) {
+  dotenv.config();
+} else {
+  // Load environment variables from the appropriate .env file
+  dotenv.config({ path: path.resolve(__dirname, `../.env.${env}`) });
+
+}
 
 // Load environment variables from .env file
 //dotenv.config();
