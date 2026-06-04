03ed010b4bccf38ec100857af86d9b33c8ff3c9f
olivierdelcroix <odelcroi@gmail.com>
2026-06-04 16:42:19 +0200

    move tests

--- tests/integration/room-access-rules/direct-room.spec.ts
+++ tests/integration/room-access-rules/direct-room.spec.ts
@@ -81,38 +81,6 @@ test.describe('API - Direct Room', () => {
     );
   });
 
-  test('Should create room on federated server', async () => {
-
-    const roomId = await createPrivateUnencryptedRoom(matrix);
-    const accessRules = await matrix.getAccessRules(roomId);
-
-    const federatedUser = await loginWithFederatedNewUser();
-
-    // Invite federatedUser into the room
-    await matrix.getClient().invite(roomId, federatedUser.mxId);
-
-    // federatedUser joins the room
-    await federatedUser.matrix.getClient().joinRoom(roomId);
-
-    // Start the client sync
-    await federatedUser.matrix.getClient().startClient();
-
-    // Verify the federated user can access the room state
-    const federatedAccessRules = await federatedUser.matrix.getAccessRules(roomId);
-    expect(federatedAccessRules).toEqual(accessRules);
-
-    // User sends a message to the room
-    const messageEventId = await matrix.getClient().sendTextMessage(roomId, 'Hello from original user');
-    expect(messageEventId).toBeDefined();
-
-    // Get the room and verify it has events
-    const room = await federatedUser.matrix.getClient().getRoom(roomId);
-    expect(room).toBeDefined();
-    const events = await room?.getLiveTimeline().getEvents();
-    expect(events && events.length).toBeGreaterThan(0);
-
-  });
-
   test.afterAll(async () => {
     await deactivateMasUser(masId);
   });
